b6a3077a12e93a85eb0e934000726b0b0e002243
Darkwood <verkadehugo@gmail.com>
2025-11-11 15:44:29 +0100

    added aurt input suport

--- main.c
+++ main.c
@@ -1,17 +1,42 @@
 #include "main.h"
 
+#define IS_FOLDER(item) ((item)->data == NULL)
+
+
 extern char __bss[], __bss_end[], __stack_top[];
+
+typedef struct {
+    uint32_t size;
+    uint8_t *data;
+} file;
+
+typedef struct item item;
+struct item {
+    char *name;
+    file *data;
+    item *next;
+    item *child;
+    uint32_t size;
+};
+
+void bronx(void) {
+    char input[128];
+    int len = get_uart_input(input, sizeof(input));
+    
+    printk("Je typte: '%s'\n", input);
+    printk("Lengte: %d chars\n", len);
+}
 
 void kernel_main(void) {
     memset(__bss, 0, ((size_t)__bss_end - (size_t)__bss));
     
     printk("Hello from RISC-V kernel!\n");
+    printk("UART initialized at 0x%x\n", UART_BASE);
     
-    uint32_t i = 0;
-    while(1) {
-        printk("Uptime is %d seconds.\n", i);
-        i++;
-        sleepk_ms(1000);
+    char *path = "/home";
+    while (1) {
+        printk("\n~%s: ",path);
+        bronx();
     }
 }
 
--- uart.h
+++ uart.h
@@ -5,9 +5,22 @@
 
 #define UART_BASE 0x10000000
 #define UART_THR  (*(volatile uint8_t *)(UART_BASE + 0))
+#define UART_RHR (*(volatile uint8_t*)(UART_BASE + 0))
+#define UART_LSR (*(volatile uint8_t*)(UART_BASE + 5))
+
+#define LSR_DATA_READY (1 << 0)
+#define LSR_TX_IDLE    (1 << 5)
 
 void uart_putc(char c);
 
 void uart_puts(const char *s) ;
+
+ void uart_putchar(char c);
+
+ char uart_getchar(void) ;
+
+uint32_t  uart_getchar_nb(char *c);
+
+uint32_t get_uart_input(char *uart_input_buffer, uint32_t max_len);
 
 #endif
