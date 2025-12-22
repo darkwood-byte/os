89a5ce46e92b5f352a9cd771a62acc0a09ad883f
Darkwood <verkadehugo@gmail.com>
2025-12-22 22:07:20 +0100

    hi

--- trap_k.c
+++ trap_k.c
@@ -1,20 +1,58 @@
 #include "trap_k.h"
 
+#define SYSCALL_PUTCHAR 1
+#define SYSCALL_GETCHAR 2
+#define SYSCALL_EXIT    3
+
+void handle_syscall(trap_frame *tf) {
+    uint32_t syscall_num = tf->a3;  // Syscall nummer staat in a3 volgens je wrapper
+    uint32_t arg0 = tf->a0;
+    uint32_t arg1 = tf->a1;
+    uint32_t arg2 = tf->a2;
+    
+    switch(syscall_num) {
+        case SYSCALL_PUTCHAR:
+            k_putchar((char)arg0);
+            tf->a0 = 0;  // Return value
+            break;
+            
+        case SYSCALL_GETCHAR:
+            tf->a0 = k_readchar();
+            break;
+            
+        case SYSCALL_EXIT:
+            k_panic("program end\n", "");
+           
+            while(1);
+            break;
+            
+        default:
+            k_printf("Unknown syscall: %d\n", syscall_num);
+            tf->a0 = 0xFFFFFFFF;;  // Error return
+            break;
+    }
+    (void)arg1;
+    (void)arg2;
+}
+
 void handle_trap(trap_frame *tf) {
     // Read traprelated CSRs
     uint32_t csr_sepc = read_csr(SEPC);
     uint32_t csr_scause = read_csr(SCAUSE);
     uint32_t csr_stval = read_csr(STVAL);
     
-    k_printf("Trap detected! scause=%x, stval=%x, sepc=%p\n", 
-             csr_scause, csr_stval, csr_sepc);
-    k_printf("Trap remains unhandled as of now. Kernel might become wonky! Consida' yo'ass warn'd!\n");
-    
+   
+    if (csr_scause == 0x08){
+        handle_syscall(tf);
+    }
+    else{
+        k_printf("Unknow trap detected! scause=%x, stval=%x, sepc=%p\n", 
+        csr_scause, csr_stval, csr_sepc);
+         k_printf("Trap remains unhandled as of now. Kernel might become wonky! Consida' yo'ass warn'd!\n");
+    }
     //skip the broken instruction
     csr_sepc += 4;
     write_csr(sepc, csr_sepc);
-    k_panic("debug\n","");
-    (void)tf; // Suppress unused parameter warning for now hehehehehehe
 }
 
 __attribute__((naked))
